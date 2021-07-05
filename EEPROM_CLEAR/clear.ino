fe02c0b4f819845229b526400d5c7e80dc0c25cb
Electromente <85633254+Electromente@users.noreply.github.com>
2021-07-05 01:40:18 -0400

    New folders

---
+++ EEPROM_CLEAR/clear.ino
@@ -0,0 +1,16 @@
+#include <EEPROM.h>
+
+void setup() {
+  // initialize the LED pin as an output.
+  pinMode(13, OUTPUT);
+  for (int i = 0 ; i < EEPROM.length() ; i++) {
+    EEPROM.write(i, 0);
+  }
+
+  // turn the LED on when we're done
+  digitalWrite(6, HIGH);
+}
+
+void loop() {
+  /** Empty loop. **/
+}
